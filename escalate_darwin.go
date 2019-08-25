a28a377f83518ef4a9c71e890f9fa1390dea3561
lu4p <lu4p@pm.me>
2019-08-25 16:07:05 +0200

    Create escalate_darwin.go

---
+++ escalate_darwin.go
@@ -0,0 +1,12 @@
+package escalate
+
+import (
+	"errors"
+	"log"
+)
+
+// Escalate // TODO: implement
+func Escalate(path string) error {
+	log.Println("Path for bypass: (", path, ")")
+	return errors.New("Not Implemented yet")
+}
