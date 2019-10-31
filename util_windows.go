6375f9b02acdd2b12fd55f521e93722afcb8b699
lu4p <lu4p@pm.me>
2019-10-31 03:55:56 +0100

    Update util_windows.go

--- util_windows.go
+++ util_windows.go
@@ -48,11 +48,3 @@ func CheckElevate() bool {
 	}
 	return true
 }
-
-// TODO: Remove Zipdir because .zip files are insecure and can be exploited
-// Zipdir archives files to zip and sends them to server
-func (c *connection) Zipdir(path string) error {
-	progress := func(archivePath string) {
-	}
-	return zip.Archive(path, c.Conn, progress)
-}
